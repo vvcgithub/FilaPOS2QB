eb14bd5c4d6c7eb7c093decc755b16d608f53676
JC Santos <jc@motley.works>
2020-11-19 15:03:57 +0800

    Console app: Initial commit

---
+++ desktop/POS2QB_Console/POS2QB_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POS2QB_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
