9c50c5c8257dbab7c578ec5f1fca4d4a65943a27
JC Santos <jc@motley.works>
2020-11-19 14:55:57 +0800

    Initial commit

---
+++ web/POS2QB_WebApp/POS2QB_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POS2QB_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
